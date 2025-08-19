012eae7a250e10b583b0931117235ea35f38c5f1
Norahmw21 <norahmw2@gmail.com>
2025-08-19 15:41:46 +0300

    Initial commit

---
+++ src/main/java/com/example/NoteFlow_Backend/NoteFlowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.NoteFlow_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteFlowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteFlowBackendApplication.class, args);
+	}
+
+}
